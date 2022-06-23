60f49bf9789c9e56f220cb56bfb36d04ed7fd20f
SophiaJAVA <107653067+SophiaJAVA@users.noreply.github.com>
2022-06-23 23:43:09 +0000

    name age java thing

--- Main.java
+++ Main.java
@@ -23,8 +23,7 @@ public class Main {
       System.out.println(age %2 == 0);
       
         // Print out whether firstName equals lastName
-      System.out.println(firstName == lastName);
-      
+      System.out.println(lastName.equals(firstName));
         // Print out whether age is greater than 0
         System.out.println(age > 0);
       
