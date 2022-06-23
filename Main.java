3fb8c78bd190bfa0c6b07f21f1c2c8d0adb17492
SophiaJAVA <107653067+SophiaJAVA@users.noreply.github.com>
2022-06-23 22:55:43 +0000

    Name Age Project

--- Main.java
+++ Main.java
@@ -1,23 +1,32 @@
 public class Main {
+  
     public static void main(String[] args) {
         // DO NOT CHANGE 
         String firstName = args[0];
         String lastName = args[1];
         int age = 0;
         try {
-            int age = Integer.parseInt(args[2]);
+            age = Integer.parseInt(args[2]);
         } catch (NumberFormatException ex) {
             ex.printStackTrace();
         }
-        
+        //System.out.println("hello world");
+        // In this print statement you can use the variables named firstName, lastName and age. To join strings together, you can use the + sign, like "Hello, I am " + firstName + " " + lastName ... (java Main.java _____ _____ ___)
+      
+      // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
+      System.out.println("Hello, I am "+ firstName + " " + lastName + ", I am " + age + " years old");
+      
+        // you are going to need another + after lastName, and age should not be in quotes because it is a variable, not a string literal
 
-        // Print out the following string: "Hello, I am <firstName> <lastName>, and I am <age> years old"
-
+      
         // Print out whether age is even
-
+      System.out.println(age %2 == 0);
+      
         // Print out whether firstName equals lastName
-
+      System.out.println(firstName == lastName);
+      
         // Print out whether age is greater than 0
-
+        System.out.println(age > 0);
+      
     }
 }
